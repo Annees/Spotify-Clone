0ab81c7c2712e8c1145f7767ad8160a38f6a0647
Annees <hashmianees97@gmail.com>
2021-09-26 12:34:50 +0530

    Final commit on spotify clone on 26-09-21

--- src/Header.js
+++ src/Header.js
@@ -1,28 +1,28 @@
-import React from 'react';
+import React from "react";
 import "./Header.css";
-import SearchIcon from "@material-ui/icons/Search";
+import { useStateValue } from "./StateProvider";
 import { Avatar } from "@material-ui/core";
-import { useDataLayerValue } from './DataLayer';
+import SearchIcon from "@material-ui/icons/Search";
 
-function Header() {
 
-    const[{ user }, dispatch] = useDataLayerValue();
+function Header({ spotify }) {
+  const [{ user }, dispatch] = useStateValue();
 
-    return (
-        <div className="header">
-            <div className="header__left">
-                <SearchIcon />
-                <input 
-                    placeholder="Search for Artists, Songs, or Podcasts"
-                    type="text"
-                />
-            </div>
-            <div className="header__right">
-                <Avatar src={user?.images[0]?.url} alt={user?.display_name} />
-                <h4>{user?.display_name}</h4>
-            </div>
-        </div>
-    )
+  return (
+    <div className="header">
+      <div className="header__left">
+        <SearchIcon />
+        <input
+          placeholder="Search for Artists, Songs, or Podcasts "
+          type="text"
+        />
+      </div>
+      <div className="header__right">
+        <Avatar alt={user?.display_name} src={user?.images[0].url} />
+        <h4>{user?.display_name}</h4>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
--- src/reducer.js
+++ src/reducer.js
@@ -1,41 +1,68 @@
-export const initialState ={
-    user:null,
-    playlists: [],
-    playing: false,
-    item: null,
-    //Remove after finished developing... just for the debugging purpose
-    // token: "BQD2CmlgELvCtsv9L3r3XWSriR5WnR_1Ggl39Y55heyspOp7NCDqSDhqsB_TFQvcu-ry494bb_tZ_413rhKFlOIkrF7m9HssOdS_oCe-7AkfQXwI9x92-ThK9lS-9TWxcrXsjWWXUJkMfQsQWewmWc0NMq7JMRfcBFv3OzRGgtQQXzb7PDJK",
+import { findAllByDisplayValue } from "@testing-library/react";
+
+export const initialState = {
+  user: null,
+  playlists: [],
+  spotify: null,
+  discover_weekly: null,
+  top_artists: null,
+  playing: false,
+  item: null,
 };
 
 const reducer = (state, action) => {
-    console.log(action);
+  console.log(action);
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
 
-    switch(action.type) {
-        case "SET_USER":
-            return {
-                ...state,
-                user: action.user,
-            };
-        case "SET_TOKEN":
-            return {
-                ...state,
-                token: action.token,
-            }
-        case "SET_PLAYLISTS":
-            return {
-                ...state,
-                playlists: action.playlists,
-            }
+    case "SET_PLAYING":
+      return {
+        ...state,
+        playing: action.playing,
+      };
 
-        case "SET_DISCOVER_WEEKLY":
-            return {
-                ...state,
-                discover_weekly: action.discover_weekly,
-            }    
-        
-        default:
-            return state;    
-    }
-}
+    case "SET_ITEM":
+      return {
+        ...state,
+        item: action.item,
+      };
+
+    case "SET_DISCOVER_WEEKLY":
+      return {
+        ...state,
+        discover_weekly: action.discover_weekly,
+      };
+
+    case "SET_TOP_ARTISTS":
+      return {
+        ...state,
+        top_artists: action.top_artists,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    case "SET_SPOTIFY":
+      return {
+        ...state,
+        spotify: action.spotify,
+      };
+
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
